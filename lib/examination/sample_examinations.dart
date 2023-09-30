1540cde678f6abc0750e6edbaee12194d192bcaa
HiroMinase <lostsymbol612@gmail.com>
2023-09-30 20:23:14 +0900

    サンプルファイルに反映

--- lib/examination/sample_examinations.dart
+++ lib/examination/sample_examinations.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'examination.dart';
+import 'examination_category.dart';
 
 class SampleExaminations extends ConsumerWidget {
   const SampleExaminations({super.key});
@@ -12,18 +13,34 @@ class SampleExaminations extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('問題一覧'),
       ),
-      body: ref.watch(examinationsFutureProvider).when(
-            data: (examinations) => ListView.builder(
-              itemCount: examinations.length,
-              itemBuilder: (context, index) => ListTile(
-                title: Text(examinations[index].question),
-                titleTextStyle: Theme.of(context).textTheme.titleMedium,
-                subtitle: Text(examinations[index].answer),
+      body: Row(
+        children: [
+          ref.watch(examinationsFutureProvider).when(
+                data: (examinations) => ListView.builder(
+                  itemCount: examinations.length,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text(examinations[index].question),
+                    titleTextStyle: Theme.of(context).textTheme.titleMedium,
+                    subtitle: Text(examinations[index].answer),
+                  ),
+                ),
+                error: (_, __) => const SizedBox(),
+                loading: () => const Center(child: CircularProgressIndicator()),
               ),
-            ),
-            error: (_, __) => const SizedBox(),
-            loading: () => const Center(child: CircularProgressIndicator()),
-          ),
+          ref.watch(examinationCategoryFutureProvider).when(
+                data: (examinations) => ListView.builder(
+                  itemCount: examinations.length,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text(examinations[index].name),
+                    titleTextStyle: Theme.of(context).textTheme.titleMedium,
+                    subtitle: Text(examinations[index].examType.displayName),
+                  ),
+                ),
+                error: (_, __) => const SizedBox(),
+                loading: () => const Center(child: CircularProgressIndicator()),
+              ),
+        ],
+      ),
     );
   }
 }
