79c855eb1046eb74869af2112d5e6923b191a9ff
HiroMinase <lostsymbol612@gmail.com>
2023-09-30 18:27:38 +0900

    ファイル名を変更

---
+++ lib/examination/sample_examinations.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'examination.dart';
+
+class SampleExaminations extends ConsumerWidget {
+  const SampleExaminations({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('問題一覧'),
+      ),
+      body: ref.watch(examinationsFutureProvider).when(
+            data: (examinations) => ListView.builder(
+              itemCount: examinations.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(examinations[index].question),
+                titleTextStyle: Theme.of(context).textTheme.titleMedium,
+                subtitle: Text(examinations[index].answer),
+              ),
+            ),
+            error: (_, __) => const SizedBox(),
+            loading: () => const Center(child: CircularProgressIndicator()),
+          ),
+    );
+  }
+}
