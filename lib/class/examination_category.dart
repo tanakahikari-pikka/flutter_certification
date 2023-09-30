b5a689a2de6ca029230aad895a057114955f6407
HiroMinase <lostsymbol612@gmail.com>
2023-09-30 17:16:09 +0900

    検定カテゴリーと検定問題のクラスを作成

---
+++ lib/class/examination_category.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ExaminationCategory {
+  const ExaminationCategory({
+    this.firestoreId,
+    required this.name,
+    required this.examType,
+    required this.rank,
+  });
+
+  final String? firestoreId;
+  final String name;
+  final String examType;
+  final int rank;
+
+  factory ExaminationCategory._fromJson(Map<String, dynamic> json) {
+    return ExaminationCategory(
+      firestoreId: json['firestoreId'] as String,
+      name: json['name'] as String,
+      examType: json['examType'] as String,
+      rank: json['rank'] as int,
+    );
+  }
+
+  factory ExaminationCategory.fromDocumentSnapshot(DocumentSnapshot ds) {
+    final data = ds.data()! as Map<String, dynamic>;
+    return ExaminationCategory._fromJson(<String, dynamic>{
+      ...data,
+      'firestoreId': ds.id,
+      'path': ds.reference.path,
+    });
+  }
+}
