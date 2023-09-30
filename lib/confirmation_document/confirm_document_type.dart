9fc94706555b11c4bbb71689271c0b9f59a49634
Tomoki <tomoki@TomokinoMacBook-Pro.local>
2023-09-30 23:05:02 +0900

    use MorkDown file as ConfirmDocument

--- lib/confirmation_document/confirm_document_type.dart
+++ lib/confirmation_document/confirm_document_type.dart
@@ -1,12 +1,22 @@
 /// 確認文書種類
 enum ConfirmDocumentType {
-  term(title: '利用規約'),
-  privacyPolicy(title: 'プライバシーポリシー');
+  term(
+    title: '利用規約',
+    filePath: 'assets/confirm_documents/term.md',
+  ),
+  privacyPolicy(
+    title: 'プライバシーポリシー',
+    filePath: 'assets/confirm_documents/privacy_policy.md',
+  );
 
   const ConfirmDocumentType({
     required this.title,
+    required this.filePath,
   });
 
   /// タイトル
   final String title;
+
+  /// ファイル保存パス
+  final String filePath;
 }
--- lib/confirmation_document/ui/confirm_document_page.dart
+++ lib/confirmation_document/ui/confirm_document_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:the_natures_app/confirmation_document/confirm_document_type.dart';
 
 class ConfirmDocumentPage extends StatelessWidget {
@@ -26,6 +28,22 @@ class ConfirmDocumentPage extends StatelessWidget {
             icon: const Icon(Icons.close),
           ),
         ],
+      ),
+      body: FutureBuilder(
+        future: rootBundle.loadString(confirmDocumentType.filePath),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<String> snapshot,
+        ) {
+          if (snapshot.hasData && snapshot.data != null) {
+            String document = snapshot.data!;
+            return Markdown(
+              data: document,
+            );
+          }
+
+          return const CircularProgressIndicator();
+        },
       ),
     );
   }
