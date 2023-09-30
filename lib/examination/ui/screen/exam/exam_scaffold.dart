46f7aa64fa941c59d1db455c2e0c50a70903803a
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-10-01 00:49:44 +0900

    Scaffold雛形

--- lib/examination/ui/screen/exam/exam_scaffold.dart
+++ lib/examination/ui/screen/exam/exam_scaffold.dart
@@ -8,90 +8,9 @@ class _Scaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            color: const Color(0xFF7DDDC0),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  "${questionAndAnswers.length - questionNumbers.length + 1} / ${questionAndAnswers.length} 問目",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Text(
-                      questionAndAnswers[currentQuestionNumber]["question"],
-                      style: const TextStyle(
-                        fontSize: 20,
-                        color: Colors.black,
-                        height: 1.5,
-                        letterSpacing: 1,
-                      )),
-                ),
-                ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(), // スクロールさせない
-                  itemCount: questionAndAnswers[currentQuestionNumber]
-                          ["choices"]
-                      .length,
-                  itemBuilder: (BuildContext context, int i) {
-                    return choiceTile(questionAndAnswers[currentQuestionNumber]
-                        ["choices"][i]);
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                const ExamScreen(),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        width: MediaQuery.of(context).size.width / 2,
-                        height: MediaQuery.of(context).size.width / 10,
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          border: Border.all(
-                            color: Colors.black45,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: const Center(
-                          child: Text(
-                            "初めからやり直す",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 60, 20, 60),
+        child: Text("data"),
       ),
     );
   }
